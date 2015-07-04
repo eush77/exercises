e77bd467672fb2b3dc503cf12383c1775169c945
Eugene Sharygin <eush77@gmail.com>
2015-07-04 17:01:06 +0300

    Solution: throttle-promises

---
+++ throttle-promises/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('babel/register');
+
+
+module.exports = function (limit, promiseFactories) {
+  return new Promise(function (resolve) {
+    var running = 0;
+    var results = Array(promiseFactories.length);
+
+    promiseFactories.splice(0, limit).forEach(start);
+    var nextIndex = limit;
+
+    function start(makePromise, index) {
+      makePromise().then(function (result) {
+        results[index] = result;
+        running -= 1;
+        if (promiseFactories.length) {
+          start(promiseFactories.shift(), nextIndex++);
+        }
+        else if (!running) {
+          resolve(results);
+        }
+      });
+      running += 1;
+    }
+  });
+};
